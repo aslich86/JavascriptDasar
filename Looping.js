09edc1fcc6b3e97b6d0a0006de748b9bc680c976
Iwan Aslich <74340158+aslich86@users.noreply.github.com>
2023-07-30 09:30:05 +0700

    Create Looping.js

---
+++ Looping.js
@@ -0,0 +1,11 @@
+// For Loop
+for (let i = 0; i < 5; i++) {
+    console.log("Iterasi ke-" + i);
+}
+
+// While Loop
+let count = 0;
+while (count < 5) {
+    console.log("Perulangan ke-" + count);
+    count++;
+}
