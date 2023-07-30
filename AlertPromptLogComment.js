aa5ac09c4550b8e33249622db5f421841181d1bb
Iwan Aslich <74340158+aslich86@users.noreply.github.com>
2023-07-30 09:27:37 +0700

    Create AlertPromptLogComment.js

---
+++ AlertPromptLogComment.js
@@ -0,0 +1,14 @@
+// Alert
+alert("Halo, ini contoh alert!");
+
+// Prompt
+let name = prompt("Masukkan nama Anda:");
+console.log("Nama Anda adalah " + name);
+
+// Log
+console.log("Pesan ini akan muncul di konsol");
+
+/* 
+    Ini adalah contoh komentar
+    multi-baris
+*/
