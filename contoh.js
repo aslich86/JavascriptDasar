8aebed1453d402759346fdb8b4653acb02f7c893
Iwan Aslich <74340158+aslich86@users.noreply.github.com>
2023-07-30 09:26:35 +0700

    Create contoh.js

---
+++ contoh.js
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Contoh Penggunaan JavaScript</title>
+    <script>
+        // Kode JavaScript Anda akan ditempatkan di sini
+    </script>
+</head>
+<body>
+    <!-- Konten HTML Anda akan ditempatkan di sini -->
+</body>
+</html>
