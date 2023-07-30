af41b030b15a5169c95c0461c262e199cfc353ad
Iwan Aslich <74340158+aslich86@users.noreply.github.com>
2023-07-30 09:29:26 +0700

    Create Array.js

---
+++ Array.js
@@ -0,0 +1,5 @@
+let fruits = ["apple", "banana", "orange"];
+console.log(fruits[0]); // Output: "apple"
+
+fruits.push("grape"); // Menambahkan nilai baru ke akhir array
+fruits.pop(); // Menghapus nilai terakhir dari array
