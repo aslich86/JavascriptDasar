011715673a35750e35f4481810a526319df095dc
Iwan Aslich <74340158+aslich86@users.noreply.github.com>
2023-07-30 09:29:07 +0700

    Create TipeDataOperator.js

---
+++ TipeDataOperator.js
@@ -0,0 +1,15 @@
+// Tipe Data
+let num = 10; // Number
+let text = "Hello"; // String
+let isTrue = true; // Boolean
+let person = { name: "John", age: 30 }; // Object
+
+// Operator
+let a = 5;
+let b = 3;
+
+let sum = a + b; // Penjumlahan
+let difference = a - b; // Pengurangan
+let product = a * b; // Perkalian
+let quotient = a / b; // Pembagian
+let remainder = a % b; // Modulo (Sisa bagi)
