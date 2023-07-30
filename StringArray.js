14dd8eb77d034d6fc734d9dbee019232ee3b3f72
Iwan Aslich <74340158+aslich86@users.noreply.github.com>
2023-07-30 09:38:01 +0700

    Create StringArray.js

---
+++ StringArray.js
@@ -0,0 +1,9 @@
+let text = "Hello, world!";
+console.log(text.length); // Output: 13
+console.log(text.toUpperCase()); // Output: "HELLO, WORLD!"
+console.log(text.split(", ")); // Output: ["Hello", "world!"]
+
+let numbers = [1, 2, 3, 4, 5];
+console.log(numbers.length); // Output: 5
+console.log(numbers.reverse()); // Output: [5, 4, 3, 2, 1]
+console.log(numbers.join("-")); // Output: "5-4-3-2-1"
